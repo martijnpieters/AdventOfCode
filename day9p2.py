3324e7bbfe70c6c2291d5bd0640d3a948f3e4a7a
Martijn Pieters <mail@martijnpieters.nl>
2015-12-09 10:00:37 +0100

    Finished day 9

---
+++ day9p2.py
@@ -0,0 +1,32 @@
+import re, itertools, sys
+from ast import literal_eval
+
+lines = [line.rstrip('\n') for line in open('day9.txt')]
+regex = re.compile(r"(\w+) to (\w+) = ([0-9]+)")
+cities = []
+dist = {}
+maxRoute = 0
+
+for s in lines:
+    m = regex.match(s)
+    if m.group(1) not in cities:
+        cities.append(m.group(1))
+    if m.group(2) not in cities:
+        cities.append(m.group(2))
+    dist[(m.group(1), m.group(2))] = int(m.group(3))
+
+for subset in itertools.permutations(cities, len(cities)):
+    route = 0
+    subset = list(subset)
+    for i in range(len(subset)-1):
+        a = subset[i]
+        b = subset[i+1]
+        if (a,b) in dist:
+            route += dist[(a,b)]
+        else:
+            route += dist[(b,a)]
+    
+    if route > maxRoute:
+        maxRoute = route
+
+print(maxRoute)
