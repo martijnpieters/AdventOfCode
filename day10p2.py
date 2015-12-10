4606309210e5a41354b254e3ed14633a506df767
Martijn Pieters <mail@martijnpieters.nl>
2015-12-10 10:46:44 +0100

    Finished day 10

---
+++ day10p2.py
@@ -0,0 +1,8 @@
+import itertools
+
+s = "3113322113"
+for i in range(50):
+    groups = [list(g) for k, g in itertools.groupby(s)]
+    s = ''.join([str(len(group)) + group[0] for group in groups])
+
+print len(s)
