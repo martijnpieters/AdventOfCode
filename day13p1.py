27f2414f1307a8d04613fce074dab26449ae10b3
Martijn Pieters <mail@martijnpieters.nl>
2015-12-21 18:18:49 +0100

    Finished day 13

---
+++ day13p1.py
@@ -0,0 +1,33 @@
+import itertools
+
+lines = [line.rstrip('\n') for line in open('day13.txt')]
+happiness = {}
+names = []
+best_score = 0
+best_permutation = None
+
+
+for line in lines:
+    name1, _, gain, amount, _, _, _, _, _, _, name2 = line.split(' ')
+    name2 = name2[:-1]
+    amount = int(amount) if gain == 'gain' else -int(amount)
+
+    happiness[(name1, name2)] = amount
+    if name1 not in names:
+        names.append(name1)
+
+for c in itertools.permutations(names):
+    l = list(c)
+    score = 0
+
+    for i in range(len(l)):
+        name1 = l[i]
+        name2 = l[(i+1) % len(l)]
+        score += happiness[(name1, name2)] + happiness[(name2, name1)]
+
+    if score > best_score:
+        best_score = score
+        best_permutation = c
+
+print(best_score)
+print(best_permutation)
